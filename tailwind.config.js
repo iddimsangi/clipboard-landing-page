901bd754f23ea4065549199c2a9fbd2624c9104c
iddimsangi <adammsangi@gmail.com>
2023-09-20 13:19:51 +0300

    feat: Add npm-run-all for concurrent script execution

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(171, 66%, 44%)",
+        "light-blue": "hsl(233, 100%, 69%)",
+        "dark-grayish-blue": "hsl(210, 10%, 33%)",
+        "grayish-blue": "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans"],
+      },
+    },
   },
   plugins: [],
-}
+};
